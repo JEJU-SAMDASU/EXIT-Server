3e6e5b4e548f0d355ed17e2912672a42640db4e0
hanbin8269 <hanbin8269@gmail.com>
2020-11-24 17:59:41 +0900

    Fix File structure, hash .. etc

--- exit_server/user/serializers.py
+++ exit_server/user/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate
 from rest_framework_jwt import utils
 from django.utils.translation import ugettext as _
 import jwt, copy
+from django.contrib.auth.hashers import make_password
 
 from .models import User, Category, AbleTime
 
@@ -12,32 +13,53 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = "__all__"
 
+
 class CreateCounselorSerializer(serializers.ModelSerializer):
     category = serializers.ListField()
     able_time = serializers.ListField()
 
-
     class Meta:
         model = User
-        fields = ("uid", "email", "password", "name", "is_counselor", "is_client", "introduction", "category", "able_time")
+        fields = (
+            "uid",
+            "email",
+            "password",
+            "username",
+            "is_counselor",
+            "is_client",
+            "introduction",
+            "category",
+            "able_time",
+        )
 
     def create(self, validated_data):
         created_validated_data = copy.deepcopy(validated_data)
-        created_validated_data.pop('category')
-        created_validated_data.pop('able_time')
+        created_validated_data.pop("category")
+        created_validated_data.pop("able_time")
+
+        created_validated_data["password"] = make_password(
+            created_validated_data["password"]
+        )
         user = User.objects.create(**created_validated_data)
 
         for j in validated_data["category"]:
+            category = None
             for i in Category.objects.all():
                 if i.subject == j:
                     break
             else:
                 category = Category.objects.create(subject=j)
-                user.category.add(category)
+            category = category or Category.objects.get(subject=j)
+            user.category.add(category)
 
-        for i, able_time in enumerate(validated_data['able_time']):
-            able_from,able_to = able_time.split("~")
-            AbleTime.objects.create(counselor_id = validated_data['uid'],day = i, able_from = able_from, able_to = able_to)
+        for i, able_time in enumerate(validated_data["able_time"]):
+            able_from, able_to = able_time.split("~")
+            AbleTime.objects.create(
+                counselor_id=validated_data["uid"],
+                day=i,
+                able_from=able_from,
+                able_to=able_to,
+            )
 
         return user
 
@@ -45,7 +67,7 @@ class CreateCounselorSerializer(serializers.ModelSerializer):
 class CreateClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("uid", "email", "password", "name")
+        fields = ("uid", "email", "password", "username")
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
@@ -59,11 +81,10 @@ class LoginCounselorSerializer(serializers.Serializer):
         credentials = {"uid": data["uid"], "password": data["password"]}
         user = authenticate(**credentials)
 
-        if user and user.is_active:
-            payload = {"uid": user.uid, "email": user.email}
+        payload = {"uid": user.uid, "email": user.email}
 
-            token = utils.jwt_encode_handler(payload)
-            return user, token
+        token = utils.jwt_encode_handler(payload)
+        return user, token
 
         msg = _("Unable to login with provided credentials")
         raise serializers.ValidationError(msg)
@@ -86,13 +107,26 @@ class LoginClientSerializer(serializers.Serializer):
         msg = _("Unable to login with provided credentials")
         raise serializers.ValidationError(msg)
 
-class CounselorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("uid", "email", "password", "name", "is_counselor", "is_client", "introduction", "category", "able_time")
+
+class CounselorSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    email = serializers.CharField()
+    username = serializers.CharField()
+    is_counselor = serializers.BooleanField()
+    is_client = serializers.BooleanField()
+    introduction = serializers.CharField()
+    category = serializers.CharField()
 
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("uid", "email", "password", "name")
+        fields = ("uid", "email", "password", "username")
+
+
+class AbleTimeSerializer(serializers.Serializer):
+    counselor = serializers.CharField()
+    day = serializers.CharField()
+    able_from = serializers.CharField()
+    able_to = serializers.CharField()
+    is_available = serializers.CharField()
