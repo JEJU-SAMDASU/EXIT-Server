83e87c52bdd8df5965286e98c5f43057dd3b1646
hanbin8269 <hanbin8269@gmail.com>
2020-11-24 17:59:59 +0900

    Fix route

--- exit_server/user/urls.py
+++ exit_server/user/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
-from .views import ClientLoginView, CounselorLoginView, ClientRegisterationView, CounselorRegisterationView
+from .views import (
+    ClientLoginView,
+    CounselorLoginView,
+    ClientRegisterationView,
+    CounselorRegisterationView,
+    UserView,
+)
 
 urlpatterns = [
-    path('counselor/sign-up/', CounselorRegisterationView.as_view()),
-    path('client/sign-up/', ClientRegisterationView.as_view()),
-    path('counselor/login/', CounselorLoginView.as_view()),
-    path('client/login/', ClientLoginView.as_view())
+    path("counselor/sign-up/", CounselorRegisterationView.as_view()),
+    path("client/sign-up/", ClientRegisterationView.as_view()),
+    path("counselor/login/", CounselorLoginView.as_view()),
+    path("client/login/", ClientLoginView.as_view()),
+    path("counselor/<uid>", UserView.as_view()),
 ]
